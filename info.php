35ed26e891a1fd979cf8f23ba1c4d4f770c21f54
Furb <jthabo@gmail.com>
2023-07-30 06:52:29 +0200

    info page

--- info.php
+++ info.php
@@ -9,32 +9,51 @@ Template Name: Info
 
 <div class="inner_wrap">
     
-  <div class="pt-20 pb-28"> 
+  <div class="pt-20 md:pt-48 pb-28"> 
 
-    <div class="pb-8">
-     <p class="label font-bold mb-4">Følg min fotokunst</p><!-- / -->
-            <p><?php echo do_shortcode('[custom-facebook-feed num=1]'); ?></p>
-  </div>
+  <div class="md:flex pb-8 md:pb-12">
 
+  <div class="left md:mr-24">
+
+  <p class="label font-bold mb-4">Følg min fotokunst</p>
+
+  <div class="flex flex-col space-y-4">
+       <a href="mailto:info@larsvandersen.com"><h2 class="underline underline-offset-8 hover:no-underline max-w-xs xl:max-w-sm">
+        Instagram</h2></a>
+        <a href="mailto:info@larsvandersen.com"><h2 class="underline underline-offset-8 hover:no-underline max-w-xs xl:max-w-sm">
+        Facebook</h2></a>
+
+  </div><!-- /.flex flex-col space-y-4 -->
+
+     
+      
+    
+
+        
+       
+    
+            
+  </div><!-- /.left -->
+  <div class="right">
+     <p class="label font-bold mb-4">Email</p><!-- / -->
+
+     
+         <a href="mailto:info@larsvandersen.com"><h2 class="underline underline-offset-8 hover:no-underline max-w-xs xl:max-w-sm">
+        info@larsvandersen.com</h2></a>
+
+
+  </div><!-- /.right -->
+
+  </div><!-- /.flex -->
+
+ 
     <div class="w-full mb-8">
          <img src="/wp-content/uploads/2023/03/lars_profil.jpg" alt="">
     </div><!-- /.flex-2 -->
      
-<div class="flex justify-between">
-   <p class=" h2 max-w-4xl pb-20"><?php the_field('om_mig_information'); ?></p>
-
-  </div>
 
 
-
-
-  
-
-
-  
-
-
-        <div class="block border-t border-b w-full pt-8 pb-8">
+        <div class="block w-full pt-8 pb-8">
             <p class="label font-bold mb-4">Samarbejde og tidligere udstlillingers</p><!-- / -->
             <?php
 
@@ -63,5 +82,8 @@ endif;
     
 
 </div><!-- /.flex -->
+
+
+
 </div><!-- /.inner_wrap -->
 <?php get_footer(); ?>
